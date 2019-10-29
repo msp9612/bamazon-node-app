82631a1527c17c8d73ffda6679ed408fa03865c7
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2019-10-29 01:35:57 -0400

    Created SQL files

---
+++ tests.sql
@@ -0,0 +1,18 @@
+DELETE FROM products;
+
+ALTER TABLE products AUTO_INCREMENT = 1;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+  ('Tablet', 'Electronics', '119.99', '13'),
+  ('Earbud Headphones', 'Electronics', '12.99', '15'),
+  ('Beard Kit', 'Beauty', '24.90', '11'),
+  ('Makeup Sponge Set', 'Beauty', '8.49', '15'),
+  ('Family Board Game', 'Toys & Games', '22.85', '2'),
+  ('Dinosaur Toy', 'Toys & Games', '42.49', '8'),
+  ('Dumbbell Set', 'Sports & Outdoors', '279.99', '20'),
+  ('Ski & Snowboard Gloves', 'Sports & Outdoors', '14.95', '4'),
+  ('Measuring Spoons Set', 'Kitchen', '10.17', '20'),
+  ('Toaster Oven', 'Kitchen', '55.24', '2');
+
+SELECT item_id, product_name, department_name, CONCAT('$', price) AS 'price', stock_quantity FROM products;
